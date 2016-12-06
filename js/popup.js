b64336ee3210afea55cb6239256b16f94ce6c00f
RouNdeL <krzys.zdulski@gmail.com>
2016-12-06 17:55:50 +0100

    Small changes to the updating mechanism

--- js/popup.js
+++ js/popup.js
@@ -355,20 +355,20 @@ function setOptions(set, options = {})
         saveOptions(settings, function()
         {
             if(savingSettings.notify)
-                chrome.tabs.query({active: true, currentWindow: true}, function(tabs)
-                {
-                    chrome.tabs.sendMessage(tabs[0].id, {type: TYPE_UPDATE_SETTINGS}, function(response)
-                    {
-                    });
-                });
+                notifyUpdate();
         });
     else if(savingSettings.notify)
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs)
+        notifyUpdate();
+}
+function notifyUpdate(callback)
+{
+    chrome.tabs.query({}, function(tabs)
+    {
+        for(let i = 0; i < tabs.length; ++ i)
         {
-            chrome.tabs.sendMessage(tabs[0].id, {type: TYPE_UPDATE_SETTINGS}, function(response)
-            {
-            });
-        });
+            chrome.tabs.sendMessage(tabs[i].id, {type: TYPE_UPDATE_SETTINGS}, callback);
+        }
+    });
 }
 function saveOptions(settings, callback)
 {
